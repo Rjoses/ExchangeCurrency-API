227a6e7b06aa4c03302a628161fa13da12adeef2
Rjoses <94126576+Rjoses@users.noreply.github.com>
2023-06-28 08:46:59 -0300

    Update CurrencyResponse.cs

--- ExchangeAPITest/Modelss/CurrencyResponse.cs
+++ ExchangeAPITest/Modelss/CurrencyResponse.cs
@@ -4,6 +4,8 @@ namespace ExchangeAPITest.Modelss
 {
     public class CurrencyResponse
     {
+        //Não tenho certeza se está correta a resposta em relação ao que precisa//
+        
         public string TimesTamp { get; set; }
         public bool Sucess { get; set; }
         public string Base { get; set; }
