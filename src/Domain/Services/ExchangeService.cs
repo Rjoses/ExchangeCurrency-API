c3dad6745e58f778d545794079ac151bba5fc310
Johnny Guielebo <jhonny@guielebo.com>
2023-06-29 00:17:03 -0300

    add coments

--- src/Domain/Services/ExchangeService.cs
+++ src/Domain/Services/ExchangeService.cs
@@ -4,6 +4,7 @@ namespace Domain.Services
 {
     public class ExchangeService : IExchangeService
     {
+        //TODO: criar servico no padrao DDD aqui e injetar na controler para usar
         public void GetRate(string baseCurrency, IList<string> symbols)
         {
             throw new NotImplementedException();
